6bbe5f1b1ad88cddedd0f286ce1983f6a8f781f8
Nevionn <initpain@proton.me>
2026-04-11 16:38:41 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,4 @@
-/**
- * reanimated/plugin по требованию библиотеки, должен идти в конце зависимостей
- */
-
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['@babel/plugin-transform-private-methods', {loose: true}],
-    'react-native-reanimated/plugin',
-  ],
+  presets: ['module:@react-native/babel-preset'],
+  plugins: ['react-native-reanimated/plugin'],
 };
